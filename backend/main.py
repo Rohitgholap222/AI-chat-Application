1fb2fd62c72f0635da1d6dc7baab17ee095aa4a3
Rohitgholap222 <rohitgholap045@gmail.com>
2026-09-03 01:19:17 +0530

    fast api project added with req file

---
+++ backend/main.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+from langchain_huggingface import (
+    ChatHuggingFace,
+    HuggingFaceEndpoint
+)
+
+from dotenv import load_dotenv
+
+# Load .env variables
+load_dotenv()
+
+app = FastAPI()
+
+# Allow React frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Request model
+class ChatRequest(BaseModel):
+    message: str
+
+
+# Hugging Face model
+llm = HuggingFaceEndpoint(
+    repo_id="openai/gpt-oss-20b",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+
+# API endpoint
+@app.post("/chat")
+def chat(request: ChatRequest):
+
+    result = model.invoke(request.message)
+
+    return {
+        "response": result.content
+    }
